d7016fefa8b9c626e3f93dccac47601e9567c2ff
SsongCh94 <thddkwl94@gmail.com>
2023-06-23 14:44:47 +0900

    메타데이터

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 import styles from './layout.module.css';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: '제품페이지',
+	description: '제품을 파는 페이지',
+};
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
 	return (
