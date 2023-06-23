76c6f914011cbcb198a9356e22c05b2ce2955162
SsongCh94 <thddkwl94@gmail.com>
2023-06-23 17:52:38 +0900

    렌더링 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Counter from '@/components/Counter';
 import Image from 'next/image';
 
 export default function Home() {
-	return <h1>홈페이지!</h1>;
+	console.log('안녕! - 서버');
+	return (
+		<>
+			<h1>홈페이지입니다.</h1>
+			<Counter />
+		</>
+	);
 }
