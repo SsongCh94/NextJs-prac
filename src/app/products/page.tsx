89b20a1574cd932205498405af828c817eff2b31
SsongCh94 <thddkwl94@gmail.com>
2023-06-27 16:26:38 +0900

    ISR, SSR 방식으로의 변환

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,6 +4,11 @@ import { getProducts } from '@/service/products';
 
 export default async function Products() {
 	const products = await getProducts();
+	const res = await fetch('https://meowfacts.herokuapp.com', {
+		next: { revalidate: 3 },
+	});
+	const data = await res.json();
+	const factText = data.data[0];
 	return (
 		<>
 			<h1>제품 소개 페이지</h1>
@@ -18,6 +23,7 @@ export default async function Products() {
 					);
 				})}
 			</ul>
+			<article className={styles.article}>{factText}</article>
 		</>
 	);
 }
